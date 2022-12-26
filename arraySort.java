8e3988ae686daba0fa997bd1a4f777779377002c
leehyeryeong <w2108@e-mirim.hs.kr>
2022-12-26 14:34:42 +0900

    일괄 커밋

--- arraySort.java
+++ arraySort.java
@@ -4,9 +4,15 @@ public class arraySort {
     public static void main(String[] args) {
         int[] arr = {3, 2, 0, 1, 4};
         //int[] idx = Arrays.binarySearch(arr, 2);
+        //System.out.println(idx);
 
         Arrays.sort(arr);
         System.out.println(Arrays.toString(arr));
         int idx = Arrays.binarySearch(arr, 2);
+        System.out.println(idx);
+
+        String[] str = {"김영희", "김철수", "김도영", "김동영"};
+        Arrays.sort(str);
+        System.out.println(str);
     }
 }
