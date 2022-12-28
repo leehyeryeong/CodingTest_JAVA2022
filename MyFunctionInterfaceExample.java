37992a7a394d3f29d88d0be7d80d3a9b04189577
leehyeryeong <w2108@e-mirim.hs.kr>
2022-12-28 15:22:57 +0900

    람다식

---
+++ MyFunctionInterfaceExample.java
@@ -0,0 +1,23 @@
+public class MyFunctionInterfaceExample {
+    public interface MyFunctionInterface {
+        public void method();
+    }
+
+    public static void main(String[] args) {
+        MyFunctionInterface fi;
+
+        fi = () -> {
+            String str = "method call1";
+            System.out.println(str);
+        };
+        fi.method();
+
+        fi = () -> {
+            System.out.println("method call2");
+        };
+        fi.method();
+
+        fi = () -> System.out.println("method call3");
+        fi.method();
+    }
+}
